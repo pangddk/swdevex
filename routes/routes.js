b3bcf675f2b5ad3c60938857d4e294ca2cb52f6e
pungpound <pungpound.kukkuk@hotmail.com>
2015-11-07 01:48:52 +0700

    first commit
    hello pung !

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 
     app.get('/', function(req, res) {
-        res.render('index', { title: '9ostrd' });
+        res.render('home');
     });
 
     // catch 404 and forward to error handler
